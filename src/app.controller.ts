3b5085abe6ab22a3d52d3fc36f7d1c2eeb6fddda
Jungwook Suh <comtrams@naver.com>
2025-11-03 14:24:36 +0900

    edit hbs/css files
    backup before adding scripts and form components

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,24 +14,68 @@ export class AppController {
   @Get('home')
   @Render('home/home')
   getHome(){
-    return { username: "수서", notificationCnt: 3};
+    return { username: "수서", notificationCnt: 3,
+      sideStyle: {
+        shared: "display:none",
+      }
+    };
   }
 
   @Get('prefs/account')
   @Render('prefs/account')
   getAcc() {
-    return { username: "수서", notificationCnt: 3};
+    return { username: "수서", notificationCnt: 3,
+      sideStyle: {
+        shared: "display:none",
+      }
+    };
   }
 
   @Get('prefs')
   @Render('prefs/prefs')
   getPrefs() {
-    return { username: "수서", notificationCnt: 3};
+    return { username: "수서", notificationCnt: 3,
+      sideStyle: {
+        shared: "display:none",
+      }
+    };
   }
 
   @Get('files/bookmarks')
-  @Render('files/bookmarks')
+  @Render('files/files')
   getBookmarks() {
-    return { username: "수서", notificationCnt: 3}
+    return { username: "수서", notificationCnt: 3, 
+      dirName: "즐겨찾기",
+      dirPath: "<a class=\"addrLink\" href=\"/files/bookmarks\">bookmarks</a>",
+      sideStyle: {
+        shared: "display:none",
+        bookmarksSel: "Sel"
+      }
+    };
+  }
+  
+  @Get('files')
+  @Render('files/files')
+  getFiles() {
+    return { username: "수서", notificationCnt: 3, 
+      dirName: "파일",
+      dirPath: "<a class=\"addrLink\" href=\"/files\">files</a>",
+      sideStyle: {
+        shared: "display:none",
+        filesSel: "Sel"
+      }
+    };
+  }
+
+  @Get('home/notifications')
+  @Render('home/notifications')
+  getNotif() {
+    return {
+      username: "수서",
+      notificationCnt: 3,
+      sideStyle: {
+        shared: "display:none"
+      }
+    }
   }
 }
